f5d98dc1f0b78c5c09b5d4c6fb8852932ffd7b2e
Rhys Godfrey <rhys@ardourdigital.co.uk>
2017-02-09 19:32:46 +0000

    Ignore errors if the search folder has been renamed already

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -2,6 +2,6 @@ Copy-Item .\Configs\connectionStrings.config .\Web\connectionStrings.config
 Copy-Item .\Configs\connectionStrings.config .\JobServer\connectionStrings.config
 Copy-Item .\Configs\communityserver_override.config .\Web\communityserver_override.config
 Copy-Item .\Configs\communityserver_override.config .\JobServer\communityserver_override.config
-Rename-Item ".\Search\copy to tomcat_lib" copy-to-tomcat_lib
+Rename-Item ".\Search\copy to tomcat_lib" copy-to-tomcat_lib -ErrorAction SilentlyContinue
 docker-compose build
 docker-compose up -d
